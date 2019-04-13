ff9a64bdc649b736ef282d4de34067dfd93117f1
julkkis666 <43990909+julkkis666@users.noreply.github.com>
2019-04-14 02:19:48 +0300

    copypasted teacher's recomended classes for networking

---
+++ Battleship/src/network/Provider3.java
@@ -0,0 +1,92 @@
+// allmänt tillgänglig på nätet, editerat från t.ex. http://zerioh.tripod.com/ressources/sockets.html
+
+package network;
+import java.io.*;
+import java.net.*;
+
+public class Provider3 {
+    ServerSocket providerSocket;
+    Socket connection = null;
+    String message;
+    PrintWriter out;
+    BufferedReader in;
+	String [] ok = new String [50];    
+	int i = 0;
+    
+    Provider3(){}
+    void run()
+    {
+        try{
+            //1. creating a server socket
+            providerSocket = new ServerSocket(2004, 10);
+            //2. Wait for connection
+            System.out.println("Waiting for connection");
+            connection = providerSocket.accept();
+            System.out.println("Connection received from " + connection.getInetAddress().getHostName() + " and accepted on " + connection.getLocalSocketAddress() + " blahaa: " + connection.getOutputStream());
+            //3. get Input and Output streams
+            in = new BufferedReader (new InputStreamReader (connection.getInputStream()));
+            out = new PrintWriter (new BufferedWriter ( new OutputStreamWriter(connection.getOutputStream())), true);
+            sendMessage("Connection successful");
+            //4. The two parts communicate via the input and output streams
+            do{
+                try{
+                	System.out.println("kommit hit");
+                    message = (String)in.readLine();
+                    System.out.println("client>" + message);
+                    while (message.equals("add")) {
+                    		sendMessage("du angav addition");                        
+                    		double x, y;
+                    		message = (String)in.readLine();
+                    		System.out.println("läst in x ");
+                    		x = Double.parseDouble(message);
+                    		sendMessage("läste in " + x);
+                    		message = (String)in.readLine();
+                    		y = Double.parseDouble(message);
+                    		System.out.println("läst in y ");
+                    		sendMessage("läste in " + y);
+                    		sendMessage("Summan av " + x + " + "+ y + " är " + (x+y));
+                        }
+  
+                    if (message.equals("bye")) sendMessage("Hejdå, det här gick ju bra");
+                }
+                catch(Exception classnot){
+                    System.err.println("Data received in unknown format");
+                }
+            }while(!message.equals("bye"));
+        }
+        catch(IOException ioException){
+            ioException.printStackTrace();
+        }
+
+        finally{
+        	System.out.println("kommit hit2");
+        	//4: Closing connection
+            try{
+                in.close();
+                out.close();
+                providerSocket.close();
+            }
+            catch(IOException ioException){
+                ioException.printStackTrace();
+            }
+        }
+    }
+    void sendMessage(String msg)
+    {
+        try{
+            out.println(msg);
+            out.flush();
+            System.out.println("server>" + msg);
+        }
+        catch(Exception ioException){
+            ioException.printStackTrace();
+        }
+    }
+    public static void main(String args[])
+    {
+        Provider3 server = new Provider3();
+        while(true){
+            server.run();
+        }
+    }
+}
---
+++ Battleship/src/network/Requester3.java
@@ -0,0 +1,111 @@
+// allmänt tillgänglig på nätet, editerat från t.ex. http://zerioh.tripod.com/ressources/sockets.html
+
+package network;
+
+import java.io.*;
+import java.net.*;
+
+public class Requester3 {
+    Socket requestSocket; 	// skapar en referens till requestSocket av typen Socket i klassen java.net
+    String server = "mymachine.abo.fi";
+    int port = 2004;
+    String message, sistaHalsning;   
+    PrintWriter output;
+	BufferedReader input;
+	BufferedReader konsolInlast;
+
+
+	
+	Requester3 () {
+	    	// tomt object requester med instansvariabler 
+    }
+	   
+public static void main(String args[]) {
+        Requester3 client = new Requester3(); // an object wi
+        client.run();
+    }
+	    
+    void run()
+    {
+        try{
+            // Skapar en socket via vilken ett försök att koppla till servern sker som ligger på mymachine... och port 2004
+            requestSocket = new Socket(server, port); // ändra detta till någon av dina maskiner
+            System.out.println("Connected to mymachine.abo.fi on port 2004 on " + requestSocket.getLocalPort());
+            // Skapar sockets för input och outputströmmarna 
+            
+            // märk!! en bufferedReader för inström, en Printwriter för utström och en bufferedreader för inläsning
+        	input = new BufferedReader (new InputStreamReader (requestSocket.getInputStream()));
+        	output = new PrintWriter(new BufferedWriter (new OutputStreamWriter (requestSocket.getOutputStream())), true);
+      	  	konsolInlast = new BufferedReader (new InputStreamReader (System.in));
+
+            // Skapar en input socket lokalt på klientmaskinen
+            do {
+                try{
+                    message = (String)input.readLine(); // läser in vad servern skickat
+                    System.out.println("server>" + message); 
+
+                    System.out.println("ange vad du vill göra (skriv alltså 'add') ");
+                    message = konsolInlast.readLine();
+                    sendMessage(message); // skickar meddelandet add till metoden sendMessage 
+                    
+                    message = (String)input.readLine(); // läser in vad servern skickat
+                    System.out.println("server>" + message);
+                    
+                    System.out.println("ange första heltalet:");
+                    message = "42";
+                    sendMessage(message); // message till metoden sendMessage 
+
+                    message = (String)input.readLine(); // läser in vad servern skickat
+                    System.out.println("server>" + message);
+                    
+                    System.out.println("ange andra heltalet:");
+                    message = konsolInlast.readLine();
+                    sendMessage(message); // message till metoden sendMessage
+                    
+                    message = (String)input.readLine(); // läser in vad servern skickat
+                    System.out.println("server>" + message);
+                    
+                    message = input.readLine(); // läser svaret
+                    System.out.println(message);
+
+                    message = "bye";  
+                    sendMessage(message); // skickar bye till metoden sendMessage
+                    
+                    sistaHalsning = input.readLine(); // läser svaret
+                    System.out.println(sistaHalsning);
+                }
+                catch(Exception e){
+                    System.err.println("data received in unknown format");
+                }
+            } while(!message.equals("bye"));
+        }
+        catch(UnknownHostException unknownHost){
+            System.err.println("You are trying to connect to an unknown host!");
+        }
+        catch(IOException ioException){
+            ioException.printStackTrace();
+        }
+        finally{
+            // Stänger av kommunikationen
+            try{
+                input.close(); // stänger in socketen
+                output.close(); // stänger out socketen
+                requestSocket.close(); // avslutar referensen 
+            }
+            catch(IOException ioException){
+                ioException.printStackTrace();
+            }
+        }
+    }
+    void sendMessage(String msg)
+    {
+        try{
+        	output.println(msg); // skriver på utkanalen meddelandet
+        	System.out.flush();
+            System.out.println("client>" + msg); // skriver ut vad klienten skickat
+        }
+        catch(Exception ioException){
+            ioException.printStackTrace();
+        }
+    }
+}
