04e7376fd833baa8d5631ec2077ea121f93d24f6
sabsaback <44085339+sabsaback@users.noreply.github.com>
2019-04-30 12:37:00 +0300

    #Piccs of ships #Now you can quit program 'CTRL+Q' and 'OK'

--- Battleship/src/window/Window.java
+++ Battleship/src/window/Window.java
@@ -1,20 +1,29 @@
 package window;
 
+import java.util.Optional;
+
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckMenuItem;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
@@ -42,17 +51,16 @@ public class Window extends Application {
 		
 	}
 	
-	//This makes everything show:
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		/*primaryStage.setScene(new Scene(Window());
 		primaryStage.show();*/
 		
 		BorderPane border = new BorderPane();		//Adding new borderPane to organize shit
-		VBox vbox = addVBoxLeft();					//Adding new VBox for my textarea (dunno if necessary)
 		
-		border.setLeft(vbox);						//Adding vBox to the left
-		border.setRight(addVBoxRight());			//Adding flowPane to right (will later show ships)
+		border.setLeft(addVBoxLeft());				//Adding vBox to the left
+		border.setRight(addVBoxRight());			//Adding vBox to right 
 		border.setCenter(addGridPane());			//Adding gridPane to center	
 		border.setTop(addMenuBar());				//Adding menuBar to top
 
@@ -67,32 +75,30 @@ public class Window extends Application {
 	//Our GridPane
 	private Parent addGridPane() {
 		
-		
 		GridPane root = new GridPane();
-		
 		
 		root.setPrefSize(300,600);							//set size of GridPane
 		root.setPadding(new Insets(10, 10, 10, 5));			//set padding around	
 		//root.setStyle("-fx-background-color: #a9a9a9;");	//Color of background
 		root.setAlignment(Pos.TOP_CENTER);					//Position of Grid
 		
-		Text text1 = new Text("Opponent");
-		text1.setFont(new Font("Agency FB", 16));
-		text1.setTranslateX(-100);
-		text1.setVisible(true);
-		root.add(text1, 0, 0);
+		Text text1 = new Text("Opponent");					//add new text
+		text1.setFont(new Font("Agency FB", 16));			//set font and size
+		text1.setTranslateX(-100);							//position X-axis
+		text1.setVisible(true);								//set visible
 		
-		createBoard1(root, -100, 20, null);
+		Text text2 = new Text("You");						//add new text
+		text2.setFont(new Font("Agency FB", 16));			//set font and size
+		text2.setTranslateX(-100);							//position X-axis
+		text2.setTranslateY(240);							//position Y-axis
+		text2.setVisible(true);								//set visible
 		
-		Text text2 = new Text("You");
-		text2.setFont(new Font("Agency FB", 16));
-		text2.setVisible(true);
-		text2.setTranslateY(240);
-		text2.setTranslateX(-100);
-		root.add(text2, 0, 0);
+		createBoard1(root, -100, 20, null);					//Creates board1
+		createBoard2(root, -100, 260);						//Creates board2
 		
-		createBoard2(root, -100, 260);
-		//TODO: Make tiles clickable
+		//add to GridPane
+		root.add(text1, 0, 0);								
+		root.add(text2, 0, 0);								
 		
 		return root;		
 	}
@@ -101,28 +107,26 @@ public class Window extends Application {
 	//Creating an VBox (VBox arranges a series of nodes in a single column)
 	public VBox addVBoxLeft() {
 		
-		Text text = new Text();
-	    TextArea log = new TextArea();
-	    VBox vBoxLeft = new VBox(text,log);
+		Text text = new Text("Log: ");
+		text.setFont(new Font("Agency FB", 20));				//set font and size of text
+		
+		TextArea log = new TextArea();
+		log.setEditable(false);									//Log not editable
 	    
-	    log.setEditable(false);
-	    
-		text.setFont(new Font("Agency FB", 20));
-		text.setText("Log: ");
-	    
-	    vBoxLeft.setPadding(new Insets(15, 15, 15, 15));
-	    vBoxLeft.setPrefSize(200, 600);
-	    vBoxLeft.setSpacing(1);
-	    vBoxLeft.setStyle("-fx-background-color: #f0f8ff;");
+		VBox vBoxLeft = new VBox(text,log);						//Putting text and log into the VBox
+	    vBoxLeft.setPadding(new Insets(15, 15, 15, 15));		//Padding around the vBox
+	    vBoxLeft.setPrefSize(200, 600);							//Preferred size
+	    vBoxLeft.setSpacing(1);									//Adding space between stuff
+	    vBoxLeft.setStyle("-fx-background-color: #f0f8ff;");	//Set background color
 
 	    
 	    //Saved in case I would need a button:
 	    
-	    /*Button buttonCurrent = new Button("Current");
-	    buttonCurrent.setPrefSize(100, 20);
+	    /*Button btnSend = new Button("Send Message");
+	    btnSend.setPrefSize(100, 20);
 	    Button buttonProjected = new Button("Projected");
 	    buttonProjected.setPrefSize(100, 20);
-	    hbox.getChildren().addAll(buttonCurrent, buttonProjected);*/
+	    vBoxLeft.getChildren().addAll(btnSend, buttonProjected);*/
 
 	    return vBoxLeft;
 	}
@@ -131,35 +135,43 @@ public class Window extends Application {
 	//Creating FlowPane (In FlowPane the nodes are laid out consecutively and wrap at the boundary set for the pane.)
 	public VBox addVBoxRight() {
 		
-		Text text = new Text();
-	    Text subHeading = new Text();
-	    Image img1 = new Image("skepp1.jpg", 100, 100, true, false);
-	    Image img2 = new Image("skepp2.jpg", 100, 100, true, false);
-	    Image img3 = new Image("skepp3.jpg", 100, 100, true, false);
-	    Image img4 = new Image("skepp4.jpg", 100, 100, true, false);
-	    Image img5 = new Image("skepp5.jpg", 100, 100, true, false);
-	    
-	    VBox vBoxRight = new VBox(text, subHeading, 
-	    						  new ImageView(img1),
-	    						  new ImageView(img2), 
-	    						  new ImageView(img3), 
-	    						  new ImageView(img4), 
-	    						  new ImageView(img5));
-	    	    
-	    //log.setEditable(false);
-	    
-		text.setFont(new Font("Agency FB", 20));
-		text.setText("Ships: ");
+		Text ships = new Text("Ships: ");							//Set heading for ships
+		ships.setFont(new Font("Agency FB", 20));					//Set font and size for heading
 		
-		subHeading.setFont(new Font("Agency FB", 14));
-		subHeading.setText("Battleship:");
+		//Should be an easier way to add lots of subHeadings, but this works
 		
-	    vBoxRight.setPadding(new Insets(15, 15, 15, 15));
-	    vBoxRight.setPrefSize(200, 600);
-	    vBoxRight.setSpacing(1);
-	    vBoxRight.setStyle("-fx-background-color: #f0f8ff;");
+		Text subHeading1 = new Text("Battleship (5 tiles) ");		//Create subHeadings for ships
+	    Text subHeading2 = new Text("Aircraft Carrier (4 tiles) ");
+	    Text subHeading3 = new Text("Submarine (3 tiles) ");
+	    Text subHeading4 = new Text("Destroyer (3 tiles) ");
+	    Text subHeading5 = new Text("Small ship (2 tiles) ");
 	    
-	   
+	    subHeading1.setFont(new Font("Agency FB", 14));				//Set fint and size for subHeadings
+		subHeading2.setFont(new Font("Agency FB", 14));
+		subHeading3.setFont(new Font("Agency FB", 14));
+		subHeading4.setFont(new Font("Agency FB", 14));
+		subHeading5.setFont(new Font("Agency FB", 14));
+		
+	    
+	    //Should be an easier way to add lots of piccs, but this works
+		
+	    Image battleship = new Image("1Battleship.jpg", 100, 100, true, false);			//Create image (inputStream, requestedWidth, requestedHeight, preserveRatio, smooth)
+	    Image aircrafCarrier = new Image("2AircraftCarrier.jpg", 100, 100, true, false);
+	    Image submarine = new Image("3Sub.jpg", 100, 100, true, false);
+	    Image destroyer = new Image("4Destroyer.jpg", 100, 100, true, false);
+	    Image smallShip = new Image("5SmallShip.jpg", 100, 100, true, false);
+	    
+	    VBox vBoxRight = new VBox(ships, subHeading1, new ImageView(battleship), 		//Putting heading, subHeadings and images into the vBox
+	    								 subHeading2, new ImageView(aircrafCarrier), 
+	    								 subHeading3, new ImageView(submarine), 
+	    								 subHeading4, new ImageView(destroyer), 
+	    								 subHeading5, new ImageView(smallShip));
+	    
+		
+	    vBoxRight.setPadding(new Insets(15, 15, 15, 15));				//Padding around the vBox
+	    vBoxRight.setPrefSize(200, 600);								//Preferred size
+	    vBoxRight.setSpacing(5);										//Adding space between stuff
+	    vBoxRight.setStyle("-fx-background-color: #f0f8ff;");			//Set background color
 	    
 	    //TODO: Put in images later like this:
 	    /*ImageView pages[] = new ImageView[8];
@@ -181,16 +193,15 @@ public class Window extends Application {
 		MenuBar menu = new MenuBar();
 		
 		//File
-		Menu file = new Menu("_File");				//Creates heading "File"
+		Menu file = new Menu("_File");					//Creates heading "File"
 		MenuItem itmNew = new MenuItem("New Game");		//Creates subHeading "New"
-		MenuItem itmOpen = new MenuItem("Open...");		//Creates subHeading "New"
-		MenuItem itmSave = new MenuItem("Save");	//Creates subHeading "Save"
-		MenuItem itmExit = new MenuItem("Exit");	//Creates subHeading "Exit"
+		MenuItem itmOpen = new MenuItem("Open...");		//Creates subHeading "Open"
+		MenuItem itmSave = new MenuItem("Save");		//Creates subHeading "Save"
+		MenuItem itmQuit = new MenuItem("Quit");		//Creates subHeading "Quit"
 		
 		
 		//Settings
 		Menu settings = new Menu("_Settings");						//Creates heading "Settings"
-		//**Not Used** MenuItem itmSettings = new MenuItem("Settings");	//Creates subHeading "Settings"
 		CheckMenuItem itmSound = new CheckMenuItem("Sound");		//Creates checkMenu "Sound"
 		CheckMenuItem itmWhatever = new CheckMenuItem("Whatever");	//Creates checkMenu "Whatever"
 		itmSound.setSelected(true);
@@ -201,7 +212,7 @@ public class Window extends Application {
 		MenuItem itmHelp = new MenuItem("Help");	//Creates subHeading "Help"
 		
 		//Add items to each menu
-		file.getItems().addAll(itmNew, itmOpen, itmSave, new SeparatorMenuItem(), itmExit);
+		file.getItems().addAll(itmNew, itmOpen, itmSave, new SeparatorMenuItem(), itmQuit);
 		settings.getItems().addAll(itmSound, new SeparatorMenuItem(), itmWhatever);
 		help.getItems().addAll(itmHelp);	
 		
@@ -213,14 +224,15 @@ public class Window extends Application {
 		file.setMnemonicParsing(true);				//Use Alt+F to open 'File' or Alt+S ti open 'Settings'
 		
 		
-		/* Could have used CONTROL_DOWN for the code that follows, but if someone is using Mac this would cause problem
+		/* 
+		 * Could have used CONTROL_DOWN for the code that follows, but if someone is using Mac this would cause problem
 		 * so therefore SHORTCUT_DOWN is a better solution. In PC it's CTRL, mac CMND etc...
 		 */
 		
 		itmNew.setAccelerator(new KeyCodeCombination(KeyCode.N, KeyCombination.SHORTCUT_DOWN));			//CTRL+N for new game	
-		itmOpen.setAccelerator(new KeyCodeCombination(KeyCode.O, KeyCombination.SHORTCUT_DOWN));		//CTRL+N for new game	
+		itmOpen.setAccelerator(new KeyCodeCombination(KeyCode.O, KeyCombination.SHORTCUT_DOWN));		//CTRL+O for open	
 		itmSave.setAccelerator(new KeyCodeCombination(KeyCode.S, KeyCombination.SHORTCUT_DOWN));		//CTRL+S for save
-		itmExit.setAccelerator(new KeyCodeCombination(KeyCode.Q, KeyCombination.SHORTCUT_DOWN));		//CTRL+Q for Exit //TODO: Remember to ask if player really want this
+		itmQuit.setAccelerator(new KeyCodeCombination(KeyCode.Q, KeyCombination.SHORTCUT_DOWN));		//CTRL+Q for Exit //TODO: Remember to ask if player really want this
 
 		itmSound.setAccelerator(new KeyCodeCombination(KeyCode.M, 
 													   KeyCombination.SHIFT_DOWN, 
@@ -254,6 +266,34 @@ public class Window extends Application {
 		    }*/
 		});
 		
+		//ActionEvent for click on 'QUit'
+		itmQuit.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				
+				Alert alert = new Alert(AlertType.INFORMATION);
+            	alert.setTitle("BattleShip");
+            	alert.setHeaderText("WARNING!!");
+            	alert.setContentText("Are you sure you wanna quit?");
+            	
+            	ButtonType buttonTypeOK = new ButtonType("OK");
+            	ButtonType buttonTypeCancel = new ButtonType("Cancel", ButtonData.CANCEL_CLOSE);
+            	alert.getButtonTypes().setAll(buttonTypeOK, buttonTypeCancel);
+
+            	
+            	Optional<ButtonType> result = alert.showAndWait();
+				if (result.get() == buttonTypeOK){
+					Platform.exit();
+				} else {
+					//TODO: How to get this to continue
+					alert.showAndWait();
+				}
+
+            	
+            	
+				
+			}
+		});
+
 		//////////////////////////////////////
 		//Some action to 'Settings'-menu	//
 		//////////////////////////////////////
