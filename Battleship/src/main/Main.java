2964ce720f33775bfc4b0b79b268861dc49b1dc2
julkkis666 <43990909+julkkis666@users.noreply.github.com>
2019-05-03 13:50:22 +0300

    re-organized main for easier teamwork

--- Battleship/src/main/Main.java
+++ Battleship/src/main/Main.java
@@ -20,7 +20,15 @@ public class Main {
 		System.out.println("Hello World!!!!!!"); //i am not useful tbh
 		
 		//Launch game window here :) (aka typ: window = new window();)
+		
 		//Window window = new Window();
+		
+		
+		
+		
+		
+		
+		
 		
 		//tempSystem for dev testing:
 		
@@ -29,102 +37,124 @@ public class Main {
 
 		//Creates ur playingfield
         myMatrix = new Matrix(10,10);
+        
+        //customize your ship layout here
         myMatrix.putHull();
+        
+        
+        //creates empty "projection" of enemy field
+        itMatrix = new Matrix(10,10);
 		
 		
 		if (host.equals("y")) {
-			//Hoster
-
-			//current model of thougth says that this should be run when waiting for a resposne from the other player
-			
-			boolean hisTurn = true;
-	        Provider3 server = new Provider3();
-	        //Requester3 client = new Requester3();
-	        
-
-	        
-	        
-	        //We'll see if this is even needed now :)))
-	        	//Temporary because i'm lazy and don't know how to do it automatically
-	        	//server.setServer(JOptionPane.showInputDialog("IP to connect to")); //IP to connect to
-	        
-	        
-	        
-	        
-	        boolean connected = false;
-	        while (!connected) {
-	        	connected = server.connect();
-	        }
-	        
-	        for (int i = 0; i < 10; i++) {
-	        	System.out.println("HIS TURN");
-	        	hisTurn = true;
-		        while(hisTurn){
-		            hisTurn = server.run();
-		        }
-		        System.out.println("UR TURN");
-		        
-		        
-		        int cordX = Integer.parseInt(JOptionPane.showInputDialog("cord X"));
-		        int cordY = Integer.parseInt(JOptionPane.showInputDialog("cord Y"));
-		        int[] cords = {cordX, cordY};
-		        
-		        //Check if cords point to a water tile on own map to avoid dumb shooting.
-		        
-		        server.run(cords);
-	        }
-	        
-
-	        server.disconnect();
-			
+			host();
 		}
 		else {
-			//Joiner
-
-	        Requester3 client = new Requester3(); // an object wi
-	        
-	        
-
-	        //Connects
-	        client.setServer(JOptionPane.showInputDialog("IP to connect to")); //IP to connect to
-	        boolean connected = false;
-	        while (!connected) {
-		        connected = client.connect();	        	
-	        }
-	        
-
-	        boolean hisTurn = false;
-	        
-	        
-	        System.out.println("UR TURN");
-	        for (int i = 0; i < 10; i++) {
-
-	        	
-		        int cordX = Integer.parseInt(JOptionPane.showInputDialog("cord X"));
-		        int cordY = Integer.parseInt(JOptionPane.showInputDialog("cord Y"));
-		        int[] cords = {cordX, cordY};
-		        
-		        //Chek if cords point to a water tile on own map to avoid dumb shooting.
-
-		        
-		        client.run(cords);
-		        
-		        
-		        System.out.println("HIS TURN");
-		        hisTurn = true;
-		        while(hisTurn){
-		            hisTurn = client.run();
-		        }
-		        System.out.println("UR TURN");
-		        
-	        }
-	        client.disconnect();
-	        
-	        
+			join();
 		}
+		
+		//end of temp system
+		
+		
 		
 		
 
 	}
+	
+	public static void host() {
+		//Hoster
+
+		//current model of thougth says that this should be run when waiting for a resposne from the other player
+		
+		boolean hisTurn = true;
+        Provider3 server = new Provider3();
+        //Requester3 client = new Requester3();
+        
+
+        
+        
+        //We'll see if this is even needed now :)))
+        	//Temporary because i'm lazy and don't know how to do it automatically
+        	//server.setServer(JOptionPane.showInputDialog("IP to connect to")); //IP to connect to
+        
+        
+        
+        
+        boolean connected = false;
+        while (!connected) {
+        	connected = server.connect();
+        }
+        
+        for (int i = 0; i < 10; i++) {
+        	System.out.println("HIS TURN");
+        	hisTurn = true;
+	        while(hisTurn){
+	            hisTurn = server.run();
+	        }
+	        System.out.println("UR TURN");
+	        
+	        
+	        int cordX = Integer.parseInt(JOptionPane.showInputDialog("cord X"));
+	        int cordY = Integer.parseInt(JOptionPane.showInputDialog("cord Y"));
+	        int[] cords = {cordX, cordY};
+	        
+	        //Check if cords point to a water tile on own map to avoid dumb shooting.
+	        
+	        server.run(cords);
+        }
+        
+
+        server.disconnect();
+		
+	}
+	
+	
+	public static void join() {
+
+		//Joiner
+
+        Requester3 client = new Requester3(); // an object wi
+        
+        
+
+        //Connects
+        client.setServer(JOptionPane.showInputDialog("IP to connect to")); //IP to connect to
+        boolean connected = false;
+        while (!connected) {
+	        connected = client.connect();	        	
+        }
+        
+
+        boolean hisTurn = false;
+        
+        
+        System.out.println("UR TURN");
+        for (int i = 0; i < 10; i++) {
+
+        	
+	        int cordX = Integer.parseInt(JOptionPane.showInputDialog("cord X"));
+	        int cordY = Integer.parseInt(JOptionPane.showInputDialog("cord Y"));
+	        int[] cords = {cordX, cordY};
+	        
+	        //Chek if cords point to a water tile on own map to avoid dumb shooting.
+
+	        
+	        client.run(cords);
+	        
+	        
+	        System.out.println("HIS TURN");
+	        hisTurn = true;
+	        while(hisTurn){
+	            hisTurn = client.run();
+	        }
+	        System.out.println("UR TURN");
+	        
+        }
+        client.disconnect();
+        
+        
+	}
+	
+	
 
 }
